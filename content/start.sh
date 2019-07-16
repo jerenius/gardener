5bbfa183a19f0a0e4582f9352fbeefb4949ad16d
Jere Virta <jerenius@gmail.com>
2019-07-16 21:04:01 +0300

    .

--- content/start.sh
+++ content/start.sh
@@ -4,7 +4,9 @@
 ln -sf /usr/bin/python3.5 /usr/bin/python
 chmod 755 /usr/src/app/temperature.py
 echo "*/1 *   *  *   *    /usr/src/app/temperature.py" >>/var/spool/cron/crontabs/root
-#node-red --settings /settings.js
+
+
+node-red --settings /settings.js
 
 
 /etc/init.d/cron start
