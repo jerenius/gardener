fcb642bd40882b4656f076733dd3f3c44c7e3b08
Jere Virta <jerenius@gmail.com>
2019-07-16 17:57:59 +0300

    foo

---
+++ content/usr/src/app/temperature.py
@@ -0,0 +1,14 @@
+import Adafruit_BMP.BMP085 as BMP085
+import os
+import paho.mqtt.client as mqtt #import the client1
+
+sensor = BMP085.BMP085()
+
+broker_address=os.environ['mqttbroker'] 
+client = mqtt.Client("P1") 
+
+temperature = '{0:0.2f}'.format(sensor.read_temperature())
+
+client.connect(broker_address) 
+client.publish("temp/gardener",temperature) 
+
