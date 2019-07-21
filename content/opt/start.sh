d8a6a8a83b50d7cfab39cb1620bc50e095c19a3f
Jere Virta <jerenius@gmail.com>
2019-07-21 19:37:43 +0300

    .

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -9,10 +9,10 @@ chmod 755 /usr/src/app/temp-loop.py
 #echo "*/1 *   *  *   *    /usr/src/app/temperature.py" >>/var/spool/cron/crontabs/root
 echo "*/2 * * * * root /usr/src/app/temperature.py" >>/etc/crontab
 
-node-red --settings /settings.js
+#node-red --settings /settings.js
 
 /usr/src/app/temperature.py 
 
 ## Start supervisord
-#/usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n &
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n &
  
