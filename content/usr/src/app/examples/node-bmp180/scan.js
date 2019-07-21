fe0272cb38d02e9669aceb0bd3245dae0c639701
Jere Virta <jerenius@gmail.com>
2019-07-21 22:47:39 +0300

    .

--- content/usr/src/app/examples/node-bmp180/scan.js
+++
@@ -1,7 +0,0 @@
-var i2c = require('i2c-bus');
-
-i2c.scan((err, data)=> {
-      data.forEach((item) =>{
-        console.log(item);
-      });
-    });
--- content/usr/src/app/examples/node-bmp180/test.js
+++
@@ -1,9 +0,0 @@
-var BMP180 = require('./index.js');
-
-var sensor = BMP180({address: 0x77,
-                     mode: 3,
-                     units: 'metric'});
-
-sensor.read(function (err, data) {
-  console.log(data);
-});
