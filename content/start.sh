7c75b2517698da8852d273e7d63cff4bbfa865c3
Jere Virta <jerenius@gmail.com>
2019-07-21 16:33:02 +0300

    .

--- content/start.sh
+++ content/start.sh
@@ -3,10 +3,10 @@
 
 ln -sf /usr/bin/python3.5 /usr/bin/python
 chmod 755 /usr/src/app/temperature.py
-echo "*/1 *   *  *   *    /usr/src/app/temperature.py" >>/var/spool/cron/crontabs/root
-
+#echo "*/1 *   *  *   *    /usr/src/app/temperature.py" >>/var/spool/cron/crontabs/root
+echo "*/2 * * * * root /usr/src/app/temperature.py" >>/etc/crontab
 
 node-red --settings /settings.js
 
 
-/etc/init.d/cron start
+cron 
--- content/usr/src/app/temperature.py
+++ content/usr/src/app/temperature.py
@@ -3,14 +3,15 @@
 import Adafruit_BMP.BMP085 as BMP085
 import os
 import paho.mqtt.client as mqtt #import the client1
+import time
 
-sensor = BMP085.BMP085()
 
-broker_address=os.environ['mqttbroker'] 
-client = mqtt.Client("P1") 
 
-temperature = '{0:0.2f}'.format(sensor.read_temperature())
-
-client.connect(broker_address) 
-client.publish("temp/gardener",temperature) 
-
+while True:
+	sensor = BMP085.BMP085()
+	broker_address=os.environ['mqttbroker'] 
+	client = mqtt.Client("P1") 
+	temperature = '{0:0.2f}'.format(sensor.read_temperature())
+	client.connect(broker_address) 
+	client.publish("temp/gardener",temperature) 
+	time.sleep(300)
