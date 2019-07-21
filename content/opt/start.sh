b7b4730f4d1a42b5e6bc8d6ef4a95572e1c9abe5
Jere Virta <jerenius@gmail.com>
2019-07-21 18:14:12 +0300

    .

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -6,7 +6,12 @@ chmod 755 /usr/src/app/temperature.py
 #echo "*/1 *   *  *   *    /usr/src/app/temperature.py" >>/var/spool/cron/crontabs/root
 echo "*/2 * * * * root /usr/src/app/temperature.py" >>/etc/crontab
 
-node-red --settings /settings.js
+#node-red --settings /settings.js
 
+#/usr/src/app/temperature.py &
 
-cron 
+## Start supervisord
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n &
+trap "supervisorctl shutdown && wait" SIGTERM
+wait
+ 
